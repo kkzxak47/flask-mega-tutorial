398479f2336d1b276d7ba274692994b1950f0296
kkzxak47 <kkzxak47@gmail.com>
2015-02-05 23:17:47 -0800

    render_template introduction

--- app/views.py
+++ app/views.py
@@ -1,6 +1,24 @@
+from flask import render_template
 from app import app
+
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return 'Hello World!'
+    user = {'nickname': 'Miguel'}  # fake user
+    title = "TTT"
+    posts = [  # fake array of posts
+        {
+            'author': {'nickname': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'nickname': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        },
+        {
+            'author': {'nickname': 'Bob'},
+            'body': 'Alice, your key has been stolen, I made a new one for ya.'
+        }
+    ]
+    return render_template("index.html", user=user, title=title, posts=posts)
