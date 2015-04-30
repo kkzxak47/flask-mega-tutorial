91afa0f370bfcc02b16be94fa2408bd7ca9cd8f3
kkzxak47 <kkzxak47@gmail.com>
2015-04-30 22:24:46 +0800

    fix duplication

--- app/forms.py
+++ app/forms.py
@@ -13,11 +13,6 @@ class EditForm(Form):
     nickname = StringField('nickname', validators=[DataRequired()])
     about_me = TextAreaField('about_me', validators=[Length(min=0, max=140)])
 
-
-class EditForm(Form):
-    nickname = StringField('nickname', validators=[DataRequired()])
-    about_me = TextAreaField('about_me', validators=[Length(min=0, max=140)])
-
     def __init__(self, original_nickname, *args, **kwargs):
         Form.__init__(self, *args, **kwargs)
         self.original_nickname = original_nickname
@@ -28,7 +23,7 @@ class EditForm(Form):
         if self.nickname.data == self.original_nickname:
             return True
         user = User.query.filter_by(nickname=self.nickname.data).first()
-        if user != None:
+        if user is not None:
             self.nickname.errors.append('This nickname is already in use. Please choose another one.')
             return False
         return True
