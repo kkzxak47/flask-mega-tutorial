abb65c3948eb855725b208b2045050a59beb6134
kkzxak47 <kkzxak47@gmail.com>
2015-04-30 22:31:35 +0800

    test and production

--- run.py
+++ run.py
@@ -4,7 +4,6 @@ import socket
 from app import app
 
 if socket.gethostname().startswith('DigitalOcean'):
-    app.run(debug=True, host='0.0.0.0')
-else:
     app.run(debug=False)
-
+else:
+    app.run(debug=True, host='0.0.0.0')
